3d7a373c1ee37b8b2bb6d44de08aff3701c32b1f
zen <0differenthang@gmail.com>
2026-02-25 21:51:05 +0700

    feat: Initialize core SCSS styling system with base styles, utilities, variables, and mixins.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "${path.resolve(__dirname, "src/assets/scss/variables").replace(/\\/g, "/")}" as *;
+          @use "${path.resolve(__dirname, "src/assets/scss/mixins").replace(/\\/g, "/")}" as *;
+        `,
+      },
+    },
+  },
 })
